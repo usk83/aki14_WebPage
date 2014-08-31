ce6daba339b9431a00e3c7986efbee1f69dc0301
usk <t13548yt@sfc.keio.ac.jp>
2014-09-01 04:02:05 +0900

    ページトップ情報を変更
    新入生向け情報→秋祭開催までの残り日数

--- js/footer.js
+++ js/footer.js
@@ -354,3 +354,19 @@ function newAnistart()
 }
 
 setTimeout("newAnistart()", 500);
+
+// remainingdays.js
+// =====================================================================
+// 秋祭開催までの残り時間表示用スクリプト
+// 9月から前日までの対応のみ
+// =====================================================================
+(function () {
+	dateObj = new Date();
+	var remainingDays = 0;
+	if (dateObj.getMonth()+1 === 9) {
+		remainingDays += 30;
+	}
+	remainingDays += 11 - dateObj.getDate();
+
+	document.getElementById("hf-contents").innerHTML = "<p>秋祭開催まで、あと<span>" + remainingDays + "</span>日</p>";
+})();
