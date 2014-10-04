3c648772cce2890211139317640d73fd5c761c6a
usk <t13548yt@sfc.keio.ac.jp>
2014-10-05 02:56:02 +0900

    企画ページ、ほぼ完成
    残り4件の紹介文
    各企画ページ

--- js/event.js
+++ js/event.js
@@ -1,17 +1,87 @@
-function loadEventDetail(place)
+var currDetail = "";
+
+function loadEventDetail(place, type)
+{
+	if (currDetail == place)
+	{
+		if(type == "linkinpage")
+		{
+			gotoDetail(place);
+			return;
+		}
+		else return;
+	}
+	else currDetail = place;
+
+	$('#planIntro button').each(function() {
+		$(this).animate({'opacity': '1'},{'duration': 200});
+	});
+	var n;
+	if (place == "stage") n = 0;
+	else if (place == "outside") n = 1;
+	else if (place == "inside") n = 2;
+	var button = "#planIntro ul li:eq(" + n + ") button";
+	$(button).animate({'opacity': '0.5'},{'duration': 200});
+
+	console.log();
+	$("#eventDetail").fadeOut(500, function(){putDetail(place, type);});
+}
+function putDetail(place, type)
 {
 	var target = "";
 	target = "./event_detail.html #event_" + place;
-	$("#eventDetail").fadeOut(500, function(){putDetail(target);});
-	// $("#fuga").fadeTo(500, 0, function(){putDetail(target);});
-}
-function putDetail(target)
-{
 	$("#eventDetail").load(target);
-	setTimeout(function(){showDetail();}, 200);
+	setTimeout(function(){showDetail(place, type);}, 200);
 }
-function showDetail()
+function showDetail(place, type)
 {
-	// $("#fuga").fadeTo(500, 1);
-	$("#eventDetail").fadeIn(500);
+	if(type == "linkinpage") $("#eventDetail").fadeIn(500, function(){gotoDetail(place);});
+	else  $("#eventDetail").fadeIn(500);
+}
+function gotoDetail(place)
+{
+	var speed = 400; // ミリ秒
+	// アンカーの値取得
+	var href= "#event_" + place;
+	// 移動先を取得
+	var target = $(href === "#" || href === "" ? 'html' : href);
+	// 移動先を数値で取得
+	// var position = target.offset().top - 52;
+
+	try{
+		var position = target.offset().top - 52;
+	} catch(e) {
+		return;
+	}
+
+	// スムーススクロール
+	$('body,html').animate({scrollTop:position}, speed, 'swing');
+
+	target.find("h1").animate({'color': '#5574CA'},{'duration': 200})
+	.animate({'color': '#fff'},{'duration': 600});
+
+	if(target.find("h1")[0] === undefined){
+		target.find("h2").animate({'color': '#5574CA'},{'duration': 200})
+		.animate({'color': '#323'},{'duration': 600});
+	}
+
+		return false;
+}
+
+(function () {
+	var randnum = Math.floor(Math.random() * 3);
+	var target = "#planIntro ul li:eq(" + randnum + ") button";
+
+	$(target).css("opacity","0.5");
+
+	if (randnum === 0) loadEventFirst('stage');
+	if (randnum === 1) loadEventFirst('outside');
+	if (randnum === 2) loadEventFirst('inside');
+})();
+function loadEventFirst(place)
+{
+	currDetail = place;
+
+	var target = "./event_detail.html #event_" + place;
+	$("#eventDetail").load(target);
 }
