70f334118ca312fede13ca0c0b0f9f0f0cf897a5
Yusuke Takahashi <t13548yt@sfc.keio.ac.jp>
2014-10-20 21:14:55 +0900

    idnex,ヘッダーのメッセージ変更
    本年度の秋祭は無事終了しました

--- js/footer.js
+++ js/footer.js
@@ -355,31 +355,6 @@ function newAnistart()
 
 setTimeout("newAnistart()", 500);
 
-// remainingdays.js
-// =====================================================================
-// 秋祭開催までの残り時間表示用スクリプト
-// =====================================================================
-(function () {
-	dateObj = new Date();
-	var remainingDays = 0;
-	if (dateObj.getMonth()+1 === 9) {
-		remainingDays += 30;
-	}
-	remainingDays += 11 - dateObj.getDate();
-	if (remainingDays > 0)
-	{
-		document.getElementById("hf-contents").innerHTML = "<p>＼ 秋祭開催まで、あと<span>" + remainingDays + "</span>日 ／</p>";
-	}
-	else if (remainingDays < -1)
-	{
-		document.getElementById("hf-contents").innerHTML = "<p>本年度の秋祭は無事終了しました</p>";
-	}
-	else
-	{
-		document.getElementById("hf-contents").innerHTML = "<p>＼ <span class='nowopen'>祝! 秋祭開催中!!!</span> ／</p>";
-	}
-})();
-
 // banner.js
 // =====================================================================
 // バナーランダム並べ替え用
